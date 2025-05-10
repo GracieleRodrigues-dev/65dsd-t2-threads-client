fbf27603e3b3c6d70f0567818a7da10cc7c18556
BrunoEspindola <brunoespindola.work@gmail.com>
2025-05-10 19:12:08 -0300

    feat: add vehicle state

--- src/modules/TrafficGrid/TrafficGrid.jsx
+++ src/modules/TrafficGrid/TrafficGrid.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, TableCell, TableRow } from './TrafficGrid.styles';
 
+const defaultVehicles = [
+  { id: 1, x: 8, y: 24 },
+  { id: 2, x: 8, y: 23 }
+];
+
 const TrafficGrid = ({ grid }) => {
+  const [vehicles, setVehicles] = useState([...defaultVehicles]);
+
+  // useEffect(() => {
+  //   const eventSource = new EventSource('http://localhost:8080/traffic/stream');
+
+  //   eventSource.addEventListener('vehicle-update', event => {
+  //     try {
+  //       const data = JSON.parse(event.data);
+  //       setVehicles(prev => {
+  //         return [...prev, data];
+  //       });
+  //     } catch (err) {
+  //       console.error('Erro ao processar evento', err);
+  //     }
+  //   });
+
+  //   eventSource.onerror = err => {
+  //     console.error('Erro na conexão SSE', err);
+  //     eventSource.close();
+  //   };
+
+  //   return () => {
+  //     eventSource.close();
+  //   };
+  // }, []);
+
+  const hasVehicle = (x, y) =>
+    vehicles.find(vehicle => vehicle.x === x && vehicle.y === y);
+
   return (
     <Table>
       <tbody>
@@ -9,7 +43,7 @@ const TrafficGrid = ({ grid }) => {
           <TableRow key={rowIndex}>
             {row.map((cell, colIndex) => (
               <TableCell key={colIndex} type={cell}>
-                {cell === 1 ? '🚘' : ''}
+                {hasVehicle(colIndex, rowIndex) ? '🚘' : ''}
               </TableCell>
             ))}
           </TableRow>
